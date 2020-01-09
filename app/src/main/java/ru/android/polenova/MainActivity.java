692b8812454d7ba3d28897b711e12a0fb24fe5f4
Marina <M.V.Polenova@users.noreply.github.com>
2020-01-09 13:14:36 +0100

    Initial commit

---
+++ app/src/main/java/ru/android/polenova/MainActivity.java
@@ -0,0 +1,51 @@
+package ru.android.polenova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Button button = findViewById(R.id.button);
+        textEdit = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToPlace();
+            }
+        });
+    }
+
+    private void sendToPlace() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String strInfo = textEdit.getText().toString();
+        if ("".equals(strInfo)) {
+            Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_SHORT).show();
+        } else {
+            char[] chars = strInfo.toCharArray();
+            boolean charBoolean = Character.isLetter(chars[0]);
+            if (charBoolean == true) {
+                intent.setData(Uri.parse(getString(R.string.place) + strInfo));
+            } else {
+                intent.setData(Uri.parse(getString(R.string.coordinate) + strInfo));
+            }
+            startActivity(intent);
+        }
+    }
+}
